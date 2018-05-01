fa4b1828daf3a404b73e1d97dcb56918a8dd5d4c
david115599 <davidbershadsky@RaspberryPi-3.local>
2018-05-01 12:55:44 -0400

    s

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ http.createServer(function (req, res) {
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+      return res.end("404 Not Found such empty");
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
